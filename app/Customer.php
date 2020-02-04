748402ac808654ba9cf35e565c7a357bd417b85b
Francisco Campos <franciiscocampos170@gmail.com>
2020-02-04 22:52:14 +0100

    adicionei novo relacionamento no customer model

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    public function country()
+    {
+        return $this->hasOne('App\Country', 'users_id');
+    }
+
+    public function company()
+    {
+        return $this->hasOne('App\Company', 'users_id');
+    }
 }
