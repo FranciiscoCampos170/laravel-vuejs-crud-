3587e4a4aa940775a4b63ed13f895bff6ba4be70
Francisco Campos <franciiscocampos170@gmail.com>
2020-02-04 22:52:30 +0100

    listando todos os customers com as tabelas relacionadas

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Company;
 use App\Customer;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index(){
-        $users = Customer::orderBy("id", "DESC")->paginate(5);
+        //$users = Customer::orderBy("id", "DESC")->paginate(5);
+        /*$users = Customer::with([
+            'company', 'country'
+        ])->get();*/
+
+        $users = DB::table('customers')
+                ->join('companies', 'customers.id', '=', 'companies.users_id')
+                ->join('countries', 'customers.id', '=', 'countries.users_id')
+                ->paginate(5);
         return response()->json($users);
+    }
+
+    public function fetch()
+    {
     }
 }
